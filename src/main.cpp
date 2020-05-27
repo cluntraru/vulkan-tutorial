241dda689fc4e9499d3d20559c2e76f0edfc1d79
Chris Luntraru <chris.luntraru@gmail.com>
2020-05-27 11:26:30 +0300

    Image views

--- src/main.cpp
+++ src/main.cpp
@@ -85,6 +85,40 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapchain();
+        createImageViews();
+    }
+
+    void createImageViews() {
+        swapchainImageViews.resize(swapchainImages.size());
+        for (size_t i = 0; i < swapchainImages.size(); ++i) {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapchainImages[i];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapchainImageFormat;
+
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            VkResult createImageViewResult = vkCreateImageView(device, &createInfo, nullptr, &swapchainImageViews[i]);
+            if (createImageViewResult != VK_SUCCESS) {
+                throw std::runtime_error("failed to create image view");
+            }
+        }
+    }
+
+    void destroyImageViews() {
+        for (auto &imageView: swapchainImageViews) {
+            vkDestroyImageView(device, imageView, nullptr);
+        }
     }
 
     void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo) {
@@ -420,6 +454,7 @@ private:
     }
 
     void cleanup() {
+        destroyImageViews();
         vkDestroySwapchainKHR(device, swapchain, nullptr);
         vkDestroyDevice(device, nullptr);
         vkDestroySurfaceKHR(instance, surface, nullptr);
@@ -471,6 +506,7 @@ private:
     VkDebugUtilsMessengerEXT debugMessenger;
 
     std::vector<VkImage> swapchainImages;
+    std::vector<VkImageView> swapchainImageViews;
 
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
